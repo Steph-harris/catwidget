09dc6fec04b8a6ad2bf40d7970df125b06e2dbaa
amberh <amberh@aweber.com>
2019-05-11 23:06:59 -0400

    consolidate, restyle, add fallback

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 
+from html import unescape
 from flask_caching import Cache
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import render_template, request, url_for
 import requests
+import yarl
 
 
 app = Flask(__name__)
@@ -15,6 +16,9 @@ app.config.from_mapping({
 })
 cache = Cache(app)
 
+BASE_URL = yarl.URL(
+    'https://api.petfinder.com/v2/animals').with_query(
+    {'organization': 'PA16', 'status': 'adoptable'})
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 STYLES = ('bulletin', 'tabs')
@@ -22,23 +26,44 @@ STYLES = ('bulletin', 'tabs')
 
 @app.route("/")
 def index():
+    page = request.args.get('page', '')
+    if page:
+        url = BASE_URL.join(yarl.URL(page))
+    else:
+        url = BASE_URL
     token_response = requests.post(
         'https://api.petfinder.com/v2/oauth2/token',
         headers={'Content-Type': 'application/x-www-form-urlencoded'},
         data={'grant_type': 'client_credentials',
               'client_id': CLIENT_ID,
-              'client_secret': CLIENT_SECRET})
-    token = token_response.json()['access_token']
-    response = requests.get(
-        'https://api.petfinder.com/v2/animals'
-        '?organization=PA16&status=adoptable',
-        headers={'Authorization': f'Bearer {token}'})
-    animals = response.json()['animals']
+              'client_secret': CLIENT_SECRET},
+        timeout=(3.05, 3))
 
-    style = request.args.get('style', '')
-    if style in STYLES:
-        return render_template('index.html', animals=animals, style=f'{style}.css')
-    return render_template('index.html', animals=animals, style='bulletin.css')
+    if token_response.status_code != 200:
+        return render_template('index.html', fallback=True)
+    else:
+        token = token_response.json()['access_token']
+        app.logger.info('Getting url %s', url)
+        response = requests.get(url,
+                                headers={'Authorization': f'Bearer {token}'},
+                                timeout=(3.05, 3))
+        if response.status_code != 200:
+            return render_template('index.html', fallback=True)
+    body = response.json()
+    animals = [{k: unescape(str(v))
+               if k in ('name', 'description') else v
+               for k, v in item.items()}
+               for item in body['animals']]
+    pagination = body.get('pagination', {}).get('_links', {})
+    prev = pagination.get('previous', {}).get('href', '')
+    next_ = pagination.get('next', {}).get('href', '')
+
+    prev_link = url_for('index', page=prev) if prev else None
+    next_link = url_for('index', page=next_) if next_ else None
+    return render_template('index.html',
+                           animals=animals,
+                           next_link=next_link,
+                           prev_link=prev_link)
 
 
 if __name__ == "__main__":
